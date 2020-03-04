1aafff7e5d82143017a49bb86df8c27c093fbcd0
budha321 <budha321@rediffmail.com>
2020-03-04 10:01:43 -0800

    test code

---
+++ Test2.py
@@ -0,0 +1,3 @@
+x=int(input("enter some number"))
+for i in range (x):
+    print ("hello Ryan")
---
+++ scratch.py
@@ -0,0 +1,11 @@
+
+Name = 'my name is Ryan'
+print (Name [::-1])
+
+Name ='my name is Ryan'
+print (len(Name))
+
+name ='my name is mohanty'
+print (name.upper())
+print (name)
+
